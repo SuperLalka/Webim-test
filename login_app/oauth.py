c1a036e7bb1ae9b525f55692fd93dd2eea5e44fb
Mark Lysenko <oomarkizoo@mail.ru>
2020-11-12 19:45:17 +0300

    Configures views for requesting & receiving auth response on github
    Also configures constants with data for requests

--- login_app/oauth.py
+++ login_app/oauth.py
@@ -1,6 +1,5 @@
-CLIENT_KEY = 'Ke1e2x1vM8K5ZmUvVixY3RvxklNaZNaVzle5Syby'
-CLIENT_SECRET = '4iSkwur3DCZ47l9ilfWBa29paMMbQYEpEYCaoqSwlANbgZynaReLcwJupKZ1Fe4BnLz8WyqvaraG8bpdKvyPKH4wr5kOh0PBQNovQd4pB9DJn26X3fDUQd6TDdW3haWT'
-REDIRECT_URI = 'http://127.0.0.1:8000/index'
+GITHUB_CLIENT_KEY = '7c443d61ceb9dfce4598'
+GITHUB_CLIENT_SECRET = '53e9232c473ca65775fd6e8e08b8b7d990f30dda'
+GITHUB_AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
 
-GITHUB_AUTHORIZE_URL = 'https://api.github.com/authorizations'
-WEBIM_TEST_AUTHORIZE_URL = 'http://127.0.0.1:8000/o/authorize/'
+REDIRECT_URI = 'http://127.0.0.1:8000/callback'
--- login_app/views.py
+++ login_app/views.py
@@ -1,5 +1,5 @@
 import random
-import requests
+from django.http import HttpResponse
 from django.shortcuts import render
 from urllib.parse import urlencode, unquote
 
@@ -7,27 +7,18 @@ from login_app import oauth
 
 
 def index(request):
-    if request.GET.get('code'):
-        request.session['user_code'] = request.GET.get('code')
     auth_params = {
-        "response_type": "code",
-        "client_id": oauth.CLIENT_KEY,
-        "redirect_uri": oauth.REDIRECT_URI,
-    }
-    return render(request, 'index.html', context={'auth_params': unquote(urlencode(auth_params))})
-
-
-def auth(request):
-    auth_params = {
-        "client_id": oauth.CLIENT_KEY,
+        "client_id": oauth.GITHUB_CLIENT_KEY,
         "state": str(random.getrandbits(64)),
         "redirect_uri": oauth.REDIRECT_URI,
-        "response_type": "code",
-        "code": request.session['user_code'],
     }
-    response = requests.post(
-        oauth.GITHUB_AUTHORIZE_URL,
-        json={**auth_params}
-    )
-    print(response)
-    return response
+    return render(request, 'index.html', context={
+        'github_auth': oauth.GITHUB_AUTHORIZE_URL,
+        'auth_params': unquote(urlencode(auth_params)),
+    })
+
+
+def callback(request):
+    request.session['user_code'] = request.GET.get('code')
+    request.session.set_expiry(10000)
+    return HttpResponse(request)
