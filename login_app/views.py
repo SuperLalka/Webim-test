ef2b7ca80ce279a453f3d2b0932029e2a40f6218
Mark Lysenko <oomarkizoo@mail.ru>
2020-11-12 14:32:22 +0300

    Configures requests for user authorization

--- login_app/views.py
+++ login_app/views.py
@@ -1,5 +1,33 @@
+import random
+import requests
 from django.shortcuts import render
+from urllib.parse import urlencode, unquote
+
+from login_app import oauth
 
 
 def index(request):
-    return render(request, 'index.html')
+    if request.GET.get('code'):
+        request.session['user_code'] = request.GET.get('code')
+    auth_params = {
+        "response_type": "code",
+        "client_id": oauth.CLIENT_KEY,
+        "redirect_uri": oauth.REDIRECT_URI,
+    }
+    return render(request, 'index.html', context={'auth_params': unquote(urlencode(auth_params))})
+
+
+def auth(request):
+    auth_params = {
+        "client_id": oauth.CLIENT_KEY,
+        "state": str(random.getrandbits(64)),
+        "redirect_uri": oauth.REDIRECT_URI,
+        "response_type": "code",
+        "code": request.session['user_code'],
+    }
+    response = requests.post(
+        oauth.GITHUB_AUTHORIZE_URL,
+        json={**auth_params}
+    )
+    print(response)
+    return response
