73b1f3fcc42fe1b5735030b2c0d0d0d3bc59043e
Mark Lysenko <oomarkizoo@mail.ru>
2020-11-12 14:26:05 +0300

    Creates a file for storing query constants

---
+++ login_app/oauth.py
@@ -0,0 +1,6 @@
+CLIENT_KEY = 'Ke1e2x1vM8K5ZmUvVixY3RvxklNaZNaVzle5Syby'
+CLIENT_SECRET = '4iSkwur3DCZ47l9ilfWBa29paMMbQYEpEYCaoqSwlANbgZynaReLcwJupKZ1Fe4BnLz8WyqvaraG8bpdKvyPKH4wr5kOh0PBQNovQd4pB9DJn26X3fDUQd6TDdW3haWT'
+REDIRECT_URI = 'http://127.0.0.1:8000/index'
+
+GITHUB_AUTHORIZE_URL = 'https://api.github.com/authorizations'
+WEBIM_TEST_AUTHORIZE_URL = 'http://127.0.0.1:8000/o/authorize/'
