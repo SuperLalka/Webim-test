11b1956c18d108fbc0e72a36593148aa06e29d0d
Mark Lysenko <oomarkizoo@mail.ru>
2020-11-16 11:19:11 +0300

    Adds a constant token receiving address on github

--- login_app/oauth.py
+++ login_app/oauth.py
@@ -1,5 +1,6 @@
 GITHUB_CLIENT_KEY = '7c443d61ceb9dfce4598'
 GITHUB_CLIENT_SECRET = '53e9232c473ca65775fd6e8e08b8b7d990f30dda'
 GITHUB_AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
+GITHUB_ACCESS_TOKEN = 'https://github.com/login/oauth/access_token'
 
 REDIRECT_URI = 'http://127.0.0.1:8000/callback'
