6538d7acaa4323ac2cf36099bef441ae5bf56a1f
Mark Lysenko <oomarkizoo@mail.ru>
2020-11-17 12:47:36 +0300

    Fixes an unhandled authentication rejection error

--- login_app/views.py
+++ login_app/views.py
@@ -3,7 +3,7 @@ import requests
 from django.shortcuts import render, redirect
 from urllib.parse import urlencode, unquote
 
-from login_app import oauth
+from django.conf import settings
 
 
 def index(request):
@@ -15,13 +15,13 @@ def index(request):
         return render(request, 'index.html')
 
     auth_params = {
-        "client_id": oauth.GITHUB_CLIENT_KEY,
+        "client_id": settings.GITHUB_CLIENT_KEY,
         "state": str(random.getrandbits(64)),
-        "redirect_uri": oauth.REDIRECT_URI,
+        "redirect_uri": settings.OAUTH_REDIRECT_URI,
     }
     request.session['user_uniq_state'] = auth_params['state']
     return render(request, 'index.html', context={
-        'github_auth': oauth.GITHUB_AUTHORIZE_URL,
+        'github_auth': settings.GITHUB_AUTHORIZE_URL,
         'auth_params': unquote(urlencode(auth_params)),
     })
 
@@ -36,21 +36,20 @@ def callback(request):
         request.session.set_expiry(10000)
 
         auth_params = {
-            "client_id": oauth.GITHUB_CLIENT_KEY,
-            "client_secret": oauth.GITHUB_CLIENT_SECRET,
+            "client_id": settings.GITHUB_CLIENT_KEY,
+            "client_secret": settings.GITHUB_CLIENT_SECRET,
             "code": request.GET.get('code'),
-            # "state": str(random.getrandbits(64)),
-            "redirect_uri": oauth.REDIRECT_URI,
+            "redirect_uri": settings.OAUTH_REDIRECT_URI,
         }
 
-        response = requests.get(oauth.GITHUB_ACCESS_TOKEN,
+        response = requests.get(settings.GITHUB_ACCESS_TOKEN,
                                 headers={"Accept": "application/json"},
                                 params={**auth_params})
 
         if response.ok:
             json_response = response.json()
             request.session['github_token'] = json_response['access_token']
-        return redirect('/')
+    return redirect('/')
 
 
 def logout(request):
