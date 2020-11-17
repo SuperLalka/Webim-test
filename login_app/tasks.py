d128523f684b2b79a1685aac9f5bd4f08d1a0065
Mark Lysenko <oomarkizoo@mail.ru>
2020-11-17 12:02:41 +0300

    Add comment to task

--- login_app/tasks.py
+++ login_app/tasks.py
@@ -7,11 +7,15 @@ from webim.celery import app
 
 @app.task
 def number_generator():
+    """
+    A task for the task manager that
+    sends a message to a single group
+    """
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(
         "main_page_num_generator",
         {
             "type": "number_message",
-            "message": random.randint(0, 100)
+            "message": random.randint(0, 10000)
         }
     )
